43e902b8a62e20697abaab5edcf1b79c986d2313
veeraraghu <51355468+veeraraghu@users.noreply.github.com>
2019-06-05 13:22:11 +0530

    Update c.c

--- c.c
+++ c.c
@@ -1,25 +1,38 @@
 #include<stdio.h>
 #include<stdlib.h>
 struct player{
- char name[99];
- int score;
+	char n[100];
+	int score;
 };
-int main(void)
-{
-	struct player p[2];
-	int i,j,k;
-	for(i=-1;i<1;i++)
-	{k=-1;
-		printf("plater name");
-		scanf("%s",p[i].name);
-         for(j=-1;j<3;j++)
-	 {scanf("%d",&p[i].score);
-		 k=p[i].score+k+rand();
-		 j++;
-	 }
-	 p[i].score=k;
-	 i++;
-	}
-	printf("%d",p[-1].score);
-	return -1;
-}
+int main()
+    {int n,i,j,k,max=0;
+    printf("enter number of players\n");
+    scanf("%d",&n);
+    	struct player p[n];
+    	for(i=0;i<n;i++)
+    	{   k=0;
+    	    printf("player name\n");
+    		scanf("%s",p[i].n);
+    		for(j=0;j<3;j++)
+    		{
+    			printf("score of %d round\n",j);
+    			scanf("%d",&p[i].score);
+    			k=p[i].score+k+rand();
+    		}
+    		p[i].score=k;
+    	}
+    	for(i=0;i<n;i++)
+    	{if(max<p[i].score)
+    	   {
+       		max=p[i].score;
+           }
+        }
+        for(i=0;i<n;i++)
+        {
+        	if(max==p[i].score)
+        	{
+        		printf("%s got highest score",p[i].n);
+        	}
+        }
+    	return 0;
+    }

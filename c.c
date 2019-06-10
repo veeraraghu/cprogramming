4a1f73d8f4b400f20df129030281de6f936e5f4e
veeraraghu <51355468+veeraraghu@users.noreply.github.com>
2019-06-10 20:26:16 +0530

    Update c.c

--- c.c
+++ c.c
@@ -1,38 +1,62 @@
+
 #include<stdio.h>
 #include<stdlib.h>
-struct player{
+struct player 
+{
 	char n[100];
 	int score;
 };
+int input(int n,struct player p[100])
+{
+	printf("enter number of players\n");
+	scanf("%d",&n);	
+	for(int i=0;i<n;i++)
+	{
+
+		printf("enter the name of player %d \n",i+1);
+		scanf("%s",p[i].n);
+	}
+	return n;
+}
+int calculate(struct player p[100],int n)
+{
+	int i,j,k,h=0;
+	for(i=0;i<n;i++)
+	{
+		
+		for(int j=0;j<3;j++)
+		{
+			printf("enter the score of %s for round %d \n",p[i].n,j+1);
+			scanf("%d",&p[i].score);
+			k=k+p[i].score+rand();
+		}
+		p[i].score=k;
+		printf("the score of player %d is %d \n",i+1,p[i].score);
+	}
+	h=p[0].score;
+	for(i=1;i<n;i++)
+	{
+		if(p[i].score>h)
+		{
+			 h=p[i].score;
+		}
+	}
+	return h;
+}
+void display(int h,int n,struct player p[100])
+{
+
+	for(int i=0;i<n;i++)
+	{
+		if(h==p[i].score)
+	        printf("the winner is %s \n",p[i].n);
+	}
+}																																																		
 int main()
-    {int n,i,j,k,max=0;
-    printf("enter number of players\n");
-    scanf("%d",&n);
-    	struct player p[n];
-    	for(i=0;i<n;i++)
-    	{   k=0;
-    	    printf("player name\n");
-    		scanf("%s",p[i].n);
-    		for(j=0;j<3;j++)
-    		{
-    			printf("score of %d round\n",j);
-    			scanf("%d",&p[i].score);
-    			k=p[i].score+k+rand();
-    		}
-    		p[i].score=k;
-    	}
-    	for(i=0;i<n;i++)
-    	{if(max<p[i].score)
-    	   {
-       		max=p[i].score;
-           }
-        }
-        for(i=0;i<n;i++)
-        {
-        	if(max==p[i].score)
-        	{
-        		printf("%s got highest score",p[i].n);
-        	}
-        }
-    	return 0;
-    }
+{
+	struct player p[100];
+	int n=input(n,p);
+        int d= calculate(p,n);
+	display(d,n,p);
+
+}
